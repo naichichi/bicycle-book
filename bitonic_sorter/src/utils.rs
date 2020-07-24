b1c22ee4ed82084bde756cdb37762e50cc27417a
naichichi <naichichi94@gmail.com>
2020-07-25 04:19:41 +0900

    update イテレータチェインでスマートに

--- bitonic_sorter/src/utils.rs
+++ bitonic_sorter/src/utils.rs
@@ -6,13 +6,8 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
     // RNGを初期化する、再現性をもたせるため毎回同じシード値を使う
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
 
-    // n個の要素が格納できるようベクタを初期化
-    let mut v = Vec::with_capacity(n);
-
-    // 0からn-1までの合計n回繰り返し乱数を生成し、ベクタに追加する
-    for _ in 0..n {
-        v.push(rng.sample(&Standard));
-    }
-
-    v
+    // rng.sample_iter()は乱数を無限に生成するイテレータを返す
+    // take(n)は元のイテレータから最初のn要素だけを取り出すイテレータを返す
+    // collect()はイテレータから値を収集して、ベクタやハッシュマップのようなコレクションに格納する
+    rng.sample_iter(&Standard).take(n).collect()
 }
