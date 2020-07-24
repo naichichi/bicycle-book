8d46b872a97184df60d2a082d6854ca46f1df350
naichichi <naichichi94@gmail.com>
2020-07-25 02:04:47 +0900

    add 単体テスト作成

--- bitonic_sorter/src/first.rs
+++ bitonic_sorter/src/first.rs
@@ -32,3 +32,34 @@ fn compare_and_swap(x: &mut [u32], up: bool) {
         }
     }
 }
+
+// #[cfg(test)]アトリュビートをつけたことで、cargo testを実行したときのみコンパイルされる
+#[cfg(test)]
+mod tests {
+    // 親モジュール(first)のsort関数を使用する
+    use super::sort;
+
+    // #[test]のついた関数はcargo testした時に実行される
+    #[test]
+    fn sort_u32_ascending() {
+        // テストデータとしてu32型のベクタを作成
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        
+        // xのスライスを作成
+        // 「&mut x」は「&mut x[..]」と書いてもいい
+        sort(&mut x, true);
+
+        // xの要素が昇順にソートされていることを確認する
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_descending() {
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        
+        sort(&mut x, false);
+
+        // xの要素が降順にソートされていることを確認する
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+}
