199e8160a749f3c5548b46c78c9965c21f565f6e
naichichi <naichichi94@gmail.com>
2020-07-25 02:30:14 +0900

    update 大小比較可能な型に限定する

--- bitonic_sorter/src/second.rs
+++ bitonic_sorter/src/second.rs
@@ -1,5 +1,6 @@
 // 型パラメータTを導入して関数をジェネリクス化する
-pub fn sort<T>(x: &mut [T], up: bool) {
+// 型パラメータTんトレイと境界Ordを追加する
+pub fn sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
         sort(&mut x[..mid_point], true);
@@ -9,7 +10,7 @@ pub fn sort<T>(x: &mut [T], up: bool) {
     }
 }
 
-fn sub_sort<T>(x: &mut [T], up: bool) {
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         compare_and_swap(x, up);
 
@@ -19,7 +20,7 @@ fn sub_sort<T>(x: &mut [T], up: bool) {
     }
 }
 
-fn compare_and_swap<T>(x: &mut [T], up: bool) {
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
