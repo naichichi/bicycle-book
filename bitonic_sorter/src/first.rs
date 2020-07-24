8611e3ebd4d56ec5d89c09e3117ec1616a47823b
naichichi <naichichi94@gmail.com>
2020-07-25 02:04:12 +0900

    fix

--- bitonic_sorter/src/first.rs
+++ bitonic_sorter/src/first.rs
@@ -26,9 +26,9 @@ fn sub_sort(x: &mut [u32], up: bool) {
 fn compare_and_swap(x: &mut [u32], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
-        if (x[i] > x[mid_point + 1]) == up {
+        if (x[i] > x[mid_point + i]) == up {
             // 要素を交換する
-            x.swap(i, mid_point + 1);
+            x.swap(i, mid_point + i);
         }
     }
 }
