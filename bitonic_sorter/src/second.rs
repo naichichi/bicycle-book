3a2adc209c93c970d1e30582f09f0e7c9076b3b0
naichichi <naichichi94@gmail.com>
2020-07-25 02:19:22 +0900

    update 文字列ソートのテストケース追加

---
+++ bitonic_sorter/src/second.rs
@@ -0,0 +1,88 @@
+// pubは他のモジュールからアクセスできることを示す
+// 引数xの型について
+//   &はポインタ経由で借用することを示す
+//   mutは値が変更可能であることを示す
+//   [u32]型はu32のスライス
+pub fn sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+
+        sub_sort(x, up)        
+    }
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            // 要素を交換する
+            x.swap(i, mid_point + i);
+        }
+    }
+}
+
+// #[cfg(test)]アトリュビートをつけたことで、cargo testを実行したときのみコンパイルされる
+#[cfg(test)]
+mod tests {
+    // 親モジュール(first)のsort関数を使用する
+    use super::sort;
+
+    // #[test]のついた関数はcargo testした時に実行される
+    #[test]
+    fn sort_u32_ascending() {
+        // テストデータとしてu32型のベクタを作成
+        // xに型注釈Vec<u32>を付ける
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        
+        // xのスライスを作成
+        // 「&mut x」は「&mut x[..]」と書いてもいい
+        sort(&mut x, true);
+
+        // xの要素が昇順にソートされていることを確認する
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_descending() {
+        // xに型注釈Vec<u32>を付ける
+        let mut x: Vec<u32>  = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        
+        sort(&mut x, false);
+
+        // xの要素が降順にソートされていることを確認する
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_string_ascending() {
+        // テストデータとして文字列型のベクタを作成
+        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
+        
+        sort(&mut x, true);
+
+        // xの要素が昇順にソートされていることを確認する
+        assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
+    }
+
+    #[test]
+    fn sort_string_descending() {
+        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
+        
+        sort(&mut x, false);
+
+        // xの要素が昇順にソートされていることを確認する
+        assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
+    }
+}
