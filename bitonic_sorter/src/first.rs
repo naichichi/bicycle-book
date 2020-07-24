7f734e668c01b9575e2d0b6d420cc6a81dcb1e55
naichichi <naichichi94@gmail.com>
2020-07-25 01:37:11 +0900

    update sort関数の本体を実装

--- bitonic_sorter/src/first.rs
+++ bitonic_sorter/src/first.rs
@@ -4,12 +4,23 @@
 //   mutは値が変更可能であることを示す
 //   [u32]型はu32のスライス
 pub fn sort(x: &mut [u32], up: bool) {
-    // 未実装の意味。コンパイルは通るが、実行するとpanicする
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+
+        sub_sort(x, up)        
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], true);
+        sub_sort(&mut x[mid_point..], false);
+    }
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
